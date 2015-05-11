3c31ae9328e80108e86e5d5e2ebbf8fef5df1097
Christian Blouin <bongotastic@gmail.com>
2015-05-11 15:19:31 -0300

    multioval script for looping

---
+++ ProgAsArt/multiovals.py
@@ -0,0 +1,24 @@
+'''
+	A dot, by you. - Python.
+	cblouin@dal.ca
+
+	Hackable: Experiment with colors, text, and the location of the elements. 
+	You can also cange the color of the background window.
+'''
+# Import everything from Graphics
+from graphics import *
+from random import randint
+
+# Build a window
+win = GraphWin('Test 1', 600, 600)
+win.setBackground('white')
+
+# Repeate someting a number of times
+for repeat in range (10):
+	poly = Oval( Point(50 + (repeat*50) ,50), Point(100 + (repeat*50), 550) )
+	poly.setFill('blue')
+	poly.draw(win)
+
+# Wait to stop
+win.getMouse()
+win.close()

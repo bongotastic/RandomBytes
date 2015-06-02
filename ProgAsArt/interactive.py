69ea845664c466c28e0f223b76c2eccf91a45969
Christian Blouin <bongotastic@gmail.com>
2015-06-02 10:29:38 -0300

    Create interactive.py
    Simple stub script for loading the module in an IDLE shell.

---
+++ ProgAsArt/interactive.py
@@ -0,0 +1,7 @@
+''' 
+    Interactive graphics setup.
+    
+    Just load this file into IDLE, run it (F5 on Windows), and access the Python console.
+'''
+# Import the graphics library so that you may use its object in the console.
+from graphics import *

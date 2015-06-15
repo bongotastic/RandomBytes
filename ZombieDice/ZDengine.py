d134e69476c260122ee38b6ca96d7c6c46a7f31c
Christian Blouin <bongotastic@gmail.com>
2015-06-15 16:16:48 -0300

    Progress on the ZD game engine

--- ZombieDice/ZDengine.py
+++ ZombieDice/ZDengine.py
@@ -12,6 +12,7 @@ class ZDGame:
 	def __init__(self):
 		# player roster
 		self.players = []
+		self.cursor = 0
 
 		# Tally
 		self.tallies = []
@@ -23,7 +24,8 @@ class ZDGame:
 		self.hand = []
 
 		# Table
-		self.table = []
+		self.brains = []
+		self.shotguns = []
 
 	def InitRound(self):
 		'''
@@ -31,8 +33,15 @@ class ZDGame:
 		'''
 		self.cup = list('GGGGGGYYYYRRR')
 
-		self.table = []
+		self.brains = []
+		self.shotguns = []
 
+	def AddPlayer(self, player):
+		self.players.append(player)
+		self.tallies.append([])
+
+	''' Mechanics that are not need for the AI
+	'''
 	def RollOnce(self, color):
 		# Get dice
 		d = ZDGame.dice[color]
@@ -48,5 +57,57 @@ class ZDGame:
 
 		self.cup = self.cup[n:]
 		return out
+
+	def FillHand(self):
+		# Refill the hand by drawing from a cup
+		self.hand.extend(self.PullFromCup( 3 - len(self.hand)))
+
+	def AleaIactaEst(self):
+		# Roll and sort each dice in hand
+		for d in self.hand:
+			outcome = self.RollOnce(d)
+
+			if outcome == 'B':
+				self.brains.append(d)
+
+			elif outcome == 'S':
+				self.shotguns.append(d)
+
+		self.hand.remove('B')
+		self.hand.remove('S')
+
+	''' Possible Action to take
+	'''
+	def RollAgain(self):
+		# AI triggers this method for another impulse
+		self.FillHand()
+		self.AleaIactaEst()
+
+	def CashIn(self):
+		# Cash in brains and move on
+		if ShotgunCount() >= 3:
+			self.tallies[self.cursor].append(0)
+		else:
+			self.tallies[self.cursor].append( self.BrainCount() )
+
+
+	''' Informational methods for the AI
+	'''
+	def BrainCount(self):
+		return len(self.brains)
+
+	def ShotgunCount(self):
+		return len(self.shotguns)
+
+
+
+
+if __name__ == "__main__":
+
+	game = ZDGame()
+
+	game.InitRound()
+
+	print game.PullFromCup(3)
 
 
